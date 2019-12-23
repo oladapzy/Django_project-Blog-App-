2d5642bf34e10db5a1e975a972af4f345a20d21f
oladapzy <dadesunloro@yahoo.com>
2019-12-23 10:01:04 -0600

    update django_heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,3 +139,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'webdev641@gmail.com'
 EMAIL_HOST_PASSWORD = 'tkyiczgvmnofgfwu'
+django_heroku.settings(locals())
